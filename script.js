e995c0c08b0d7df917b755b3c8ba8053f62898cf
Marjolaine Paz <marjolaine.paz@hotmail.fr>
2018-10-28 11:32:35 +0100

    Added Arrow Keys navigation / deleted home WIP message

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function load_content(src){
 		if(xhr.readyState==4 && xhr.status==200){
 			document.getElementById("pageContent").innerHTML=xhr.responseText;
 			document.body.classList.remove("home");
+      currentPage=src;
 		}
 	};
 	xhr.open("GET",src,true);
@@ -12,4 +13,46 @@ function load_content(src){
 
 function goHome(){
 	document.body.classList.add("home");
+}
+
+
+/* ----ARROW KEYS NAVIGATION---- */
+
+function nomdelafonction(event) {
+  console.log(event)
+  if(event.keyCode==39) { //39=rightArrow
+    goRight()
+  }
+   if(event.keyCode==37) { //37=lefttArrow
+   goLeft()
+  }
+}
+
+var projects=[
+"works_content/Absolver.html",
+"works_content/RoH.html",
+"works_content/Apoptosis.html",
+"works_content/MLD.html"]
+var currentPage
+
+function goRightLeft(offset) {
+  var index=projects.indexOf(currentPage)
+  if (index >= 0) {
+    index=index+offset
+    if (index >= projects.length) {
+      index=0
+   }
+   if (index <0) {
+     index=projects.length-1
+   }
+    load_content(projects[index])
+  }
+}
+
+function goRight() {
+  goRightLeft(+1)
+}
+
+function goLeft() {
+  goRightLeft(-1)
 }
